470b2d615ac527bc4d1f53e1322dcfecbd99c3b6
Hena Ramay <henaramay@Henas-MacBook-Air.local>
2024-01-09 15:32:54 -0700

    final simple version

--- game.py
+++ game.py
@@ -52,23 +52,24 @@ def change_garray(garray,grid):
 
 
 
+# Set up for receiving arguments
+msg="Please specify the following:"
+parser=argparse.ArgumentParser(description=msg, )
+parser.add_argument("--grid", nargs=2, type=int, help="provide two integers with a space to define the grid x y",  required=True )
+parser.add_argument("--probability", type=float, help="A number between 0-1",action=range_action(0, 1),  required=True )
 
-msg="Please specify the following  grid size for game of life \n"
-parser=argparse.ArgumentParser(description=msg)
-parser.add_argument("--grid", nargs=2, type=int, help="provide two integers with a space to define the grid x y")
-parser.add_argument("--probability", type=float, help="A number between 0-1",action=range_action(0, 1))
-#parser.add_argument("--number", type=int, help="an integer number")
+
 args=vars(parser.parse_args())
 grid=args['grid']
 prob=args['probability']
 
 # create a array of size grid
-
 garray=np.zeros(grid)
 print(grid[0],grid[1],prob)
 total_ones=int(prob*(grid[0]*grid[1]))
 print(total_ones)
 
+## Generating 5 times more random numbers so that duplicates can be removed
 x=np.random.randint( 0,high=garray.shape[0],size=total_ones*5)
 y=np.random.randint( 0,high=garray.shape[1],size=total_ones*5)
 # Assign 1s randomly to the grid
@@ -77,15 +78,16 @@ print([x,y])
 
 coord = list(set(tuple(zip(x, y))))
 
-
+# Just a precautionary check to see if 
 if(len(coord) < total_ones):
-    print("Error, not enough unique ")
+    print("Error, not enough unique grid cells produced. ")
     exit(1)
 
 print(len(coord))
 
 coord=coord[0:total_ones]
 
+# replace the zeros with ones in garray for the selected coordinates
 for x,y in coord:
     print(x,y)
     garray[x,y]=1
@@ -96,7 +98,7 @@ print(garray)
 
 
 
-
+## Function requred for animation
 def update(frame,matrix,grid):
     # Update the matrix with new random values
     print("grid",grid)
@@ -106,14 +108,10 @@ def update(frame,matrix,grid):
     mat.set_array(matrix)
     return matrix
      
-#    x+=1
-#    print(garray)
-#print("dfsd",change_garray(garray,grid))
+
+# Set up the plot
 fig, ax = plt.subplots()
 mat = ax.matshow(garray, cmap='viridis')
-#print(mat)
-#print("Test")
-#plt.colorbar(mat)
 ani = animation.FuncAnimation(fig, update,fargs=(garray,grid),frames=10)
 #plt.show()
 
